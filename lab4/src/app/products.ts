a2b685d9babfe02b16a35540b3d3e12b292da784
aisana <a_ondassyn@kbtu.kz>
2024-02-21 11:33:42 +0600

    task

---
+++ lab4/src/app/products.ts
@@ -0,0 +1,98 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  //image: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'iPhone 15',
+    price: 717,
+    description:
+      'Apple iPhone 15 - смартфон, сочетающий в себе передовую оптику, мощный процессор, долгоиграющую батарею и запоминающийся дизайн.',
+    image:
+      'https://resources.cdn-kaspi.kz/img/m/p/he2/h1d/83559338442782.png?format=gallery-medium',
+  },
+  {
+    id: 2,
+    name: 'AirPods 2nd generation',
+    price: 119,
+    description: 'A great headphones and easy to use.',
+  },
+  {
+    id: 3,
+    name: 'USB Type-C - Lightning',
+    price: 5,
+    description: 'Necessary accessory for Apple ecossystem.',
+  },
+  {
+    id: 4,
+    name: 'Apple EarPods Lightning',
+    price: 18,
+    description: 'Cool headphones',
+  },
+  {
+    id: 5,
+    name: 'Apple Watch SE 40 мм',
+    price: 285,
+    description:
+      'Apple Watch Series SE - часы, с которыми Вы оцените каждую секунду.',
+  },
+  {
+    id: 6,
+    name: 'Apple Pencil 2nd Generation',
+    price: 129,
+    description: 'Awesome stilus for your iPad.',
+  },
+  {
+    id: 7,
+    name: 'iPad 2021 Wi-Fi 10.2 in',
+    price: 359,
+    description:
+      'Мощный. Простой в использовании и универсальный. iPad создан специально для ваших любимых занятий. Работайте, играйте, творите, учитесь, общайтесь и делайте множество других дел.',
+  },
+  {
+    id: 8,
+    name: 'AirPods Max',
+    price: 599,
+    description:
+      'Bluetooth гарнитура Apple AirPods Max сможет полностью изменить ваше представление о полноразмерных наушниках.',
+  },
+  {
+    id: 9,
+    name: 'Magic Mouse 2 (USB-C)',
+    price: 69,
+    description:
+      'Мышь Magic Mouse 2 получила не только новый дизайн, но и возможность перезарядки. А это значит, что вам больше не нужны обычные батарейки.',
+  },
+  {
+    id: 10,
+    name: 'MacBook Air 13 MGND3',
+    price: 919,
+    description:
+      'Невероятно тонкий (всего 41 мм в самом тонком месте) и лёгкий (1,29 кг), он работает совершенно бесшумно, а для зарядки компьютера достаточно адаптер мощностью 30 Вт.',
+  },
+  {
+    id: 11,
+    name: 'Apple Lightning - 3.5 mm jack 0.07 м',
+    price: 2.5,
+    description:
+      'Этот адаптер позволяет подключать аксессуары с разъёмом 3,5 мм к устройствам с разъёмом Lightning.',
+  },
+  {
+    id: 12,
+    name: 'Apple Airtag',
+    price: 30,
+    description:
+      'AirTag — это суперлёгкий способ находить свои вещи. Прикрепите один трекер AirTag к ключам, а другой — к рюкзаку. И теперь их видно на карте в приложении «Локатор».',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
